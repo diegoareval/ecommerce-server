a2ea21362f3f6ab88ed22ca71ac38ef494300c82
Diego <diego2000avelar@gmail.com>
2021-01-09 15:49:11 -0600

    obteniendo genero seleccioando

--- src/resolvers/query/genre.ts
+++ src/resolvers/query/genre.ts
@@ -1,3 +1,5 @@
+import { COLLECTIONS } from './../../config/constants';
+import { findOneElement } from './../../lib/db-operations';
 import { IResolvers } from "graphql-tools";
 import GenreService from '../../services/genre.service';
 
@@ -5,6 +7,13 @@ const resolversGenreQuery: IResolvers = {
   Query: {
     async genres(_, __, { db }){
       return new GenreService(_,__, {db}).items();
+    },
+    async genre(_, {id}, {db}){
+     return {
+         status: true,
+         message: "hi",
+         genre: findOneElement(COLLECTIONS.GENRES, db, {id})
+     }
     }
   },
 };
