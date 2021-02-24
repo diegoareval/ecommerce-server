7166e5c8bc39fb3c5a2f63df9cbc98a6253f71e7
Diego <diego2000avelar@gmail.com>
2021-02-24 11:16:11 -0600

    it adds transport nodemailer

---
+++ src/config/mailer.ts
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+
+export const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.USER_EMAIL, // generated ethereal user
+      pass: process.env.USER_PASSWORD, // generated ethereal password
+    },
+})
+
+transport.verify().then(()=>{
+    console.log("verificacion correcta");
+}).catch(()=>{
+    console.log("verificacion incorrecta");
+});
