1a5ae6950df3a516f9996ad6602936166098e644
Diego <diego2000avelar@gmail.com>
2021-02-25 21:48:23 -0600

    activating user with error

--- src/interfaces/jwt.interface.ts
+++ src/interfaces/jwt.interface.ts
@@ -1,5 +1,7 @@
 import { IUser } from "./user.interface";
 
 export  interface IJwt {
+    id?: string
     user: IUser
+    email?: String;
 }
--- src/resolvers/mutation/email.ts
+++ src/resolvers/mutation/email.ts
@@ -1,12 +1,12 @@
+import { EXPIRETIME } from './../../config/constants';
 import { IResolvers } from "graphql-tools";
 import { transport } from "../../config/mailer";
+import JWT from "../../lib/jwt";
 
 // mutation para registrar usuario
 const resolversEmailMutation: IResolvers = {
   Mutation: {
     sendEmail(_, {mail}) {
-      console.log(mail);
-      
       return new Promise((resolve, reject) => {
         // send mail with defined transport object
         transport.sendMail({
@@ -27,6 +27,27 @@ const resolversEmailMutation: IResolvers = {
         });
       });
     },
+    activeUserEmail(_, {id, email}){
+      return new Promise((resolve, reject) => {
+        // send mail with defined transport object
+        const token = new JWT().sign({id, email}, EXPIRETIME.H1);
+        transport.sendMail({
+          from: '"Ecommerce 👻" <diego2000avelar@gmail.com>', // sender address
+          to: email, // list of receivers
+          subject: "Activar Usuario", // Subject line
+          html: "", // html body
+        }, (error, _)=> {
+            (error)?reject({
+              status: false,
+              message: error
+            }): resolve({
+              status: true,
+              message: "correo enviado correctamente a: "+ email,
+              mail: null
+            })
+        });
+      });
+    }
   },
 };
 
