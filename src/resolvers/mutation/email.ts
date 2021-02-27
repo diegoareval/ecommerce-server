4a1f996bdb7960056d7311f8a427ac42f400a7fe
Diego <diego2000avelar@gmail.com>
2021-02-27 14:01:14 -0600

    add email

--- src/resolvers/mutation/email.ts
+++ src/resolvers/mutation/email.ts
@@ -35,12 +35,11 @@ const resolversEmailMutation: IResolvers = {
       });
     },
     async activeUserEmail(_, { id, email }) {
+      const token = new JWT().sign({ user: { id, email } }, EXPIRETIME.H1);
+      const html = `Para activar la cuenta haz click sobre esto: <a href="${process.env.CLIENT_URL}/#/active/${token}"> Click aqui</a>`;
       return new Promise((resolve, reject) => {
         // send mail with defined transport object
-        const token = new JWT().sign({ user: { id, email } }, EXPIRETIME.H1);
-        console.log(token);
-
-        const html = `Para activar la cuenta haz sobre esto: <a href="${process.env.CLIENT_URL}/#/active/${token}"> Click aqui</a>`;
+      
         transport.sendMail(
           {
             from: '"Ecommerce 👻" <diego2000avelar@gmail.com>', // sender address
@@ -99,11 +98,38 @@ const resolversEmailMutation: IResolvers = {
           user: null,
         };
       }
-      return {
-        status: true,
-        message: `el usuario con el ${email} existe`,
-        user: null,
-      };
+      const newUser = {
+        id: user.id,
+        email
+      }
+      const token = new JWT().sign({user: newUser}, EXPIRETIME.M15);
+      console.log(token);
+      
+      const html = `Para cambiar la contraseña haz click sobre esto: <a href="${process.env.CLIENT_URL}/#/reset/${token}"> Click aqui</a>`;
+      return new Promise((resolve, reject) => {
+        // send mail with defined transport object
+      
+        transport.sendMail(
+          {
+            from: '"Ecommerce 👻" <diego2000avelar@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Cambiar Contraseña", // Subject line
+            html, // html body
+          },
+          (error, _) => {
+            error
+              ? reject({
+                  status: false,
+                  message: error,
+                })
+              : resolve({
+                  status: true,
+                  message: "correo enviado correctamente a: " + email,
+                  mail: null,
+                });
+          }
+        );
+      });
       
     },
   },
